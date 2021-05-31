604b01a46983d9c91630a2ad62ae27376e305d78
= <=>
2021-05-31 21:49:51 +0300

    fix script

--- src/script.sql
+++ src/script.sql
@@ -8,3 +8,28 @@ CREATE TABLE IF NOT EXISTS users (
     role INT NOT NULL,
     status INT NOT NULL DEFAULT 0
     );
+
+
+CREATE TABLE IF NOT EXISTS courses
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL UNIQUE,
+    description VARCHAR(255) NOT NULL,
+    duration    LONG         NOT NULL,
+    level       INT          NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS themes
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL UNIQUE
+    );
+
+CREATE TABLE IF NOT EXISTS courseAssigment
+(
+    user_id INT REFERENCES users (id) ON DELETE CASCADE,
+    course_id INT REFERENCES courses (id) ON DELETE CASCADE,
+    courseStatus INT NOT NULL,
+    mark INT NOT NULL,
+    UNIQUE (user_id, course_id)
+    );
